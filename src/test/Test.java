c5626b46fced961643ca406bb82bff25e6766283
Dylan Marvin-150003044 <150003044@vusdnoc.vusd.k12.ca.us>
2017-10-31 10:22:24 -0700

    class

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,14 +4,15 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-        //Blokus Startus
+        
     }
 
 }
-//I love chicken
-/*
-Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang (Gucci gang!)
-Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang (Gucci gang!)
-Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang (Gucci gang!)
-Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang, Gucci gang (Gucci gang!)
-*/
+
+class Board{
+    
+    Board(){
+    
+}
+    
+}
